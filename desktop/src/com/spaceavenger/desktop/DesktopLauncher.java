ff5887a242b7c7b03466adfe16740335ad9e8ecc
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2014-11-23 21:17:37 +0300

    initial

---
+++ desktop/src/com/spaceavenger/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.spaceavenger.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.spaceavenger.SpaceAvenger;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceAvenger(), config);
+	}
+}
