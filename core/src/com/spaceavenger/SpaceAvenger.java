b6bea94d41f332f8f10bbad8c2e3b4d12130331e
Maxim Pyatishev <m.pyatishev@dlinksys.ru>
2014-11-24 16:37:50 +0300

    probes

--- core/src/com/spaceavenger/SpaceAvenger.java
+++ core/src/com/spaceavenger/SpaceAvenger.java
@@ -2,26 +2,63 @@ package com.spaceavenger;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input.Buttons;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+
+class MyInputAdapter extends InputAdapter {
+	@Override
+	public boolean keyUp(int keyCode) {
+		return false;
+	}
+
+	@Override
+	public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+		if (button == Buttons.LEFT) {
+		}
+
+		return false;
+	}
+}
 
 public class SpaceAvenger extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private Texture avengerImage;
+	private OrthographicCamera camera;
+	private Rectangle avenger;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		avengerImage = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, 800, 480);
+
+		avenger = new Rectangle(800 / 2 - 64 / 2, 20, 64, 64);
+
+		Gdx.input.setInputProcessor(new MyInputAdapter());
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		camera.update();
+
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(avengerImage, avenger.x, avenger.y);
 		batch.end();
+	}
+
+	@Override
+	public void dispose() {
+		avengerImage.dispose();
+		batch.dispose();
 	}
 }
--- desktop/src/com/spaceavenger/desktop/DesktopLauncher.java
+++ desktop/src/com/spaceavenger/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import com.spaceavenger.SpaceAvenger;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Space Avenger";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new SpaceAvenger(), config);
 	}
 }
